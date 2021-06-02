9dfdaadd9dd723469ad6bf0dc5260ecece261059
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2021-06-02 11:25:18 +0100

    Adição de 'PROPS' para representar dados

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import React from 'react';
 import Tabela from './Tabela'
 // importar o componente 'Tabela2'
 import Tabela2 from './Tabela2'
+// importar o componente 'Tabela3'
+import Tabela3 from './Tabela3'
+
 
 // cria o 'componente' App
 // neste caso é criada uma FUNÇÃO
@@ -15,21 +18,59 @@ class App extends React.Component {
   // todas as classes terão, obrigatoriamente,
   // um método chamado 'render'
   render() {
+
+    // esta variável é imutável
+    // e, essa é uma das características dos PROPS
+    const alunos = [
+      {
+        nome: "João",
+        apelido: "Lopes"
+      },
+      {
+        nome:"Luísa",
+        apelido:"Matos"
+      },
+      {
+        nome: "Mário",
+        apelido: "Santos"
+      },
+      {
+        nome: "Ana",
+        apelido: "Silva"
+      }
+    ]
+
+
     // todo o componente tem obrigatoriamente de 'devolver' alguma coisa
     return (
-      <div className="container"> {/* e apenas consegue devolver um ÚNICO objeto */}
-        <h1>Lista de <i>tabelas</i> obtidas de diversas formas...</h1>
+      <div className="container" > {/* e apenas consegue devolver um ÚNICO objeto */}
+        < h1 > Lista de <i> tabelas</i > obtidas de diversas formas...</h1 >
 
         <h2>Tabela puramente 'hard coded'</h2>
         <Tabela />
         <br />
+
         {/* junção de dois componentes dentro de um outro */}
         <h2>Tabela com componentes simples</h2>
         <Tabela2 />
-      </div>
+
+        {/* uso de PROPS
+             um PROP é uma 'propriedade' que se atribui a uma variável
+             de certa forma, irá funcionar como o ViewBag da .NET */}
+        <br />
+        <h2>Tabela com dados 'definidos externamente'</h2>
+        {/* 'dadosAlunos' é uma variável de entrada no componente
+              Naturalmente, do lado do componente existirá um parâmetro para receber estes dados */}
+        {/*  {alunos} - 'alunos' será o nome dos dados
+                        está escrita entre chavetas {} pq é a forma de se aceder ao
+                        conteúdo de variáveis, dentro do JSX */}
+        <Tabela3 dadosAlunos={alunos} />
+        <br />
+      </div >
     );
   }
 }
 
 // um componente só é acessível se for 'exportado'
 export default App;
+
---
+++ src/Tabela3.js
@@ -0,0 +1,57 @@
+// Tabela3.js
+// ****************************************************** 
+
+import React from 'react'
+
+// função que devolve o Cabeçalho da tabela
+function CabecalhoTabela() {
+    return (
+        <thead>
+            <tr>
+                <td>Nome</td>
+                <td>Apelido</td>
+            </tr>
+        </thead>
+    )
+}
+
+// definição da função que devolve o Corpo da tabela
+// faz exatamente o mesmo da linha 7
+const CorpoTabela = (props) => {
+    // esta função 'interna' irá ler e processar todos
+    // os objetos definidos dentro do array 'dadosRecebidos'
+    const rows = props.dadosRecebidos.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.nome}</td>
+                <td>{row.apelido}</td>
+            </tr>
+        )
+    })
+
+    // valor devolvido pela função 'CorpoTabela'
+    return (<tbody>{rows}</tbody>)
+}
+
+// componente que junta os dois sub-componentes, formando um novo 'componente'
+class Tabela3 extends React.Component {
+    render() {
+
+        // estamos a ler os dados que são recebidos pelo componente
+        // <=> this.props.dadosAlunos
+        const { dadosAlunos } = this.props
+
+        return (
+            <table className="table table-striped table-success">
+                <CabecalhoTabela />
+                {/* o parâmetro 'dadosRecebidos' irá ajudar ao processamento
+                    dos dados que vêm da componente 'mãe' */}
+                <CorpoTabela dadosRecebidos={dadosAlunos} />
+            </table>
+        )
+    }
+}
+
+
+export default Tabela3
+
